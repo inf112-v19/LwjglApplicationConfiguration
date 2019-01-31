8001c8acd7342aff76c8cda00fc4c032b8e7a1d1
qez008 <morten_bergmann@live.no>
2019-01-31 14:26:19 +0100

    silly board test

---
+++ src/test/java/inf112/skeleton/app/BoardTest.java
@@ -0,0 +1,14 @@
+package inf112.skeleton.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class BoardTest {
+
+    @Test
+    public void sillyBoardTest(){
+        //Todo:
+        fail();
+    }
+}
