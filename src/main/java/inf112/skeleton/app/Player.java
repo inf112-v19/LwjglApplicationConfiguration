6769ddacc0d666b641bfbf2935a13485c00d903c
qez008 <morten_bergmann@live.no>
2019-02-02 20:10:03 +0100

    Program registers in Player class and Player test

--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -1,6 +1,10 @@
 package inf112.skeleton.app;
 
+import org.lwjgl.Sys;
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Player {
 
@@ -8,19 +12,25 @@ public class Player {
     private int x;
     private int y;
 
+    private int lives;
+    private int dmg;
+
+
     //Which direction the player is currently facing.
     private Direction direction;
 
     //All cards in the player hand.
     private ArrayList<ProgramCardCD> cardDeck;
 
-    private ArrayList<ProgramCardCD> chosenCards;
+    private ProgramCardCD[] registers = new ProgramCardCD[5];
+    private boolean[] lockedRegisters = new boolean[5];
 
     //How many cards you have at hand to pick from
     private int cardLimit;
 
     //How many cards you can choose
     private int chosenCardsLimit;
+
 
     /**
      * Initialize a new player (a new robot).
@@ -42,6 +52,8 @@ public class Player {
         this.cardLimit = 9;
         this.chosenCardsLimit = 5;
 
+        dmg = 0;
+        lives = 3;
     }
 
     /**
@@ -66,14 +78,66 @@ public class Player {
         this.cardLimit = 9;
         this.chosenCardsLimit = 5;
 
+        dmg = 0;
+        lives = 3;
+    }
+
+    public void takeDamage(){
+        dmg++;
+        if(isDestroyed()){
+            lives--;
+            repairDamage();
+            if(lives == 0);
+                // TODO: GAME OVER for this player.
+            else;
+                // TODO: Move to last backup
+        } else if(dmg >= 5)
+            lockedRegisters[9-dmg] = true;
+    }
+
+    /* Registers are stored like this:
+     * [0][1][2][3][4]
+     *
+     * taking 5 dmg locks register 4.
+     * 6 dmg locks 3..
+     */
+    public boolean isLocked(int register){
+        return lockedRegisters[register];
+    }
+
+    /**
+     * When damage is taken some registers might become locked.
+     * Cards in locked registers should remain untill the register is unlocked.
+     *
+     * @return list of cards that are not locked in registers
+     */
+    public ArrayList<ProgramCardCD> returnCards(){
+        ArrayList<ProgramCardCD> list = new ArrayList<>();
+        list.addAll(cardDeck);
+        for(int register = 0; register < 5; register++) {
+            if (isLocked(register)) return list; // if one register is locked, all the registers after it
+            list.add(registers[register]);      // are locked too.
+        }
+        return list;
+    }
+
+    public boolean isDestroyed(){
+        return dmg > 9;
+    }
+
+    public void repairDamage(){
+        dmg = 0;
+        for(boolean b : lockedRegisters)
+            b = false;
     }
 
     public void chooseCard(int cardID){
-        if(chosenCards.size() >= chosenCardsLimit){
+        if(registers.length >= chosenCardsLimit){
             System.out.println("Already have maximum amount of cards chosen");
             return;
         }
-        chosenCards.add(cardDeck.get(cardID));
+        // TODO:
+//        chosenCards.add(cardDeck.get(cardID));
     }
 
     /**
@@ -100,7 +164,8 @@ public class Player {
      *          the cards chosen this round
      */
     public ArrayList<ProgramCardCD> getChosenCards(){
-        return this.chosenCards;
+        // TODO:
+        return null;
     }
 
     /**
@@ -226,10 +291,6 @@ public class Player {
         //Texture/sprite is updated automatically because of render method in GameScreen.java.
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setX(int x) {
         this.x = x;
     }
@@ -245,4 +306,22 @@ public class Player {
     public int getY() {
         return y;
     }
+
+    public int getDamage(){
+        return dmg;
+    }
+
+    public ArrayList<ProgramCardCD> getRegisters(){
+        ArrayList<ProgramCardCD> list = new ArrayList<>();
+        for(ProgramCardCD pc : registers)
+            list.add(pc);
+
+        return list;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLives() { return this.lives; }
 }
---
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -0,0 +1,98 @@
+package inf112.skeleton.app;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class PlayerTest {
+    Player player;
+
+    @Before
+    public void setup(){
+        player = new Player("testBot", 0,0, Direction.SOUTH);
+    }
+
+    @Test
+    public void takeDamageTest(){
+        player.takeDamage();
+        assertEquals(1, player.getDamage());
+    }
+
+    @Test
+    public void takeDamageTest2(){
+        int expected = 1;
+        for(int i = 0; i < 9; i++){
+            player.takeDamage();
+            assertEquals(expected++, player.getDamage());
+        }
+    }
+
+    @Test
+    public void zeroDmgDoesNotDestroy(){
+        assertEquals(3, player.getLives());
+        assert(!player.isDestroyed());
+    }
+
+    @Test
+    public void nineDmgDoesdNotDestroy(){
+        assertEquals(3, player.getLives());
+        for(int i = 0; i < 9; i++){
+            player.takeDamage();
+        }
+        assertEquals(3, player.getLives());
+        assert(!player.isDestroyed());
+    }
+
+    @Test
+    public void tenDmgDestroys(){
+        assertEquals(3, player.getLives());
+        for(int i = 0; i < 10; i++){
+            player.takeDamage();
+        }
+        assertEquals(2,player.getLives());
+    }
+
+    @Test
+    public void fiveDmgLocksOneRegister(){
+        for(int i = 0; i < 5; i++)
+            player.takeDamage();
+        assert(player.isLocked(4));
+
+        //the other registers should not be locked:
+        for(int i = 0; i < 4; i++)
+            assert(!player.isLocked(i));
+    }
+
+    @Test
+    public void nineDmgLocksAllRegisters(){
+        for(int i = 0; i < 9; i++)
+            player.takeDamage();
+        //all registers should be locked:
+        for(int i = 0; i < 5; i++)
+            assert(player.isLocked(4-i));
+    }
+
+    @Test
+    public void repairResultsInZeroDmg(){
+        for(int i = 0; i < 9; i++)
+            player.takeDamage();
+        player.repairDamage();
+        assertEquals(0, player.getDamage());
+        for(int i = 0; i < 5; i++)
+            assert(player.isLocked(i));
+    }
+
+    @Test
+    public void returnCardsTest(){
+        // TODO:
+        fail();
+    }
+
+    @Test
+    public void lockedCardsAreNotReturned(){
+        // TODO:
+        fail();
+    }
+}
