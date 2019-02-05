cd4b40a473ee5beb35b46ed03befc0ae5dccb382
simen <simenlien1@hotmail.com>
2019-02-05 23:16:15 +0100

    Added a few more tests

--- src/test/java/inf112/skeleton/app/MovePlayerTest.java
+++ src/test/java/inf112/skeleton/app/MovePlayerTest.java
@@ -6,66 +6,66 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class BoardTest {
+public class MovePlayerTest {
 
     public static Player player;
     private int x = 300; //Starting x pos
     private int y = 450; //Starting y pos
 
     @Before
-    public void initialize(){
-        player = new Player("Player1", x,y,Direction.SOUTH);
+    public void initialize() {
+        player = new Player("Player1", x, y, Direction.SOUTH);
     }
 
 
     @Test
-    public void move1ForwardWorks(){
+    public void move1ForwardWorks() {
         player.move(1);
-        assertEquals(y+150, player.getY());
+        assertEquals(y + 150, player.getY());
     }
 
     @Test
-    public void move2ForwardWorks(){
+    public void move2ForwardWorks() {
         player.move(2);
-        assertEquals(y+300, player.getY());
+        assertEquals(y + 300, player.getY());
     }
 
     @Test
-    public void move3ForwardWorks(){
+    public void move3ForwardWorks() {
         player.move(3);
-        assertEquals(y+450, player.getY());
+        assertEquals(y + 450, player.getY());
     }
 
     @Test
-    public void rotateLeftMove1(){
+    public void rotateLeftMove1() {
         player.rotate(Rotate.LEFT);
         player.move(1);
-        assertEquals(x+150, player.getX());
+        assertEquals(x + 150, player.getX());
     }
 
 
     @Test
-    public void move1EastThen1West(){
+    public void move1EastThen1West() {
         player.rotate(Rotate.LEFT);
         player.move(1);
         player.rotate(Rotate.UTURN);
         player.move(1);
-        assertEquals(x,player.getX());
+        assertEquals(x, player.getX());
     }
 
     @Test
-    public void Move1ThenRotateLeftThenMove2ThenRotateLeftMove1(){
+    public void Move1ThenRotateLeftThenMove2ThenRotateLeftMove1() {
         player.move(1);
         player.rotate(Rotate.LEFT);
         player.move(2);
         player.rotate(Rotate.LEFT);
         player.move(1);
-        assertEquals(x+300, player.getX());
+        assertEquals(x + 300, player.getX());
         assertEquals(y, player.getY());
     }
 
     @Test
-    public void rotateLeftMove3ThenRotateRightMove2ThenRotateUturnMove2(){
+    public void rotateLeftMove3ThenRotateRightMove2ThenRotateUturnMove2() {
         player.rotate(Rotate.LEFT);
         player.move(3);
         player.rotate(Rotate.RIGHT);
@@ -73,10 +73,51 @@ public class BoardTest {
         player.rotate(Rotate.UTURN);
         player.move(2);
 
-        assertEquals(x+450, player.getX());
+        assertEquals(x + 450, player.getX());
         assertEquals(y, player.getY());
     }
 
+    @Test
+    public void RotateRightTwice() {
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        assertEquals(player.getDirection(), Direction.NORTH);
+    }
+
+    @Test
+    public void rotateRightThreeTimes() {
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        assertEquals(player.getDirection(), Direction.EAST);
+    }
+
+    @Test
+    public void rotateRightFourTimes(){
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        assertEquals(player.getDirection(), Direction.SOUTH);
+    }
+
+    @Test
+    public void rotateRightFiveTimes(){
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        player.rotate(Rotate.RIGHT);
+        assertEquals(player.getDirection(), Direction.WEST);
+    }
+
+    @Test
+    public void doUturnThreeTimes(){
+        player.rotate(Rotate.UTURN);
+        player.rotate(Rotate.UTURN);
+        player.rotate(Rotate.UTURN);
+        assertEquals(player.getDirection(), Direction.NORTH);
+    }
     //...
 
 }
