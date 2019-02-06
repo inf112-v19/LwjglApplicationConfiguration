bba669c41c119fbc6ef8ccf06504699265a09e3d
qez008 <morten_bergmann@live.no>
2019-02-06 17:15:52 +0100

    textures.png file for board textures. added deadly lasers

---
+++ assets/gameboard/roborallyTextures.tsx
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="roborallyTextures" tilewidth="32" tileheight="32" spacing="1" tilecount="30" columns="6">
+ <image source="textures.png" width="197" height="164"/>
+ <tile id="1">
+  <properties>
+   <property name="Wall" value="W"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Wall" value="S"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Wall" value="E"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Wall" value="N"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Hole" value=""/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Wall" value="S, W"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Wall" value="N, W"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Wall" value="N, E"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Wall" value="S, E"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Wall" value="N, S"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Wall" value="E, W"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="Wall" value="N, S, E, W"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Wall" value=""/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Wall" value="N, S, E "/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Wall" value="N, E, W"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="Wall" value="N, S, W"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Wall" value="S, E, W"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="Belt" value="N"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Belt" value="E"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Belt" value="S"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="Belt" value="W"/>
+  </properties>
+ </tile>
+</tileset>
