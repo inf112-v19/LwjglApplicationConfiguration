b22a6e0c7ca94a718c81d3ec10d94be025e8baaf
Gunnar <Gunnar.Kleiven@student.uib.no>
2019-03-01 13:54:01 +0100

    foolish test for playerRepairdamage

--- src/test/java/inf112/roborally/game/RepairSiteTest.java
+++ src/test/java/inf112/roborally/game/RepairSiteTest.java
@@ -2,21 +2,34 @@ package inf112.roborally.game;
 
 import inf112.roborally.game.objects.Player;
 import inf112.roborally.game.objects.RepairSite;
+import inf112.roborally.game.world.Board;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Quite foolish test for player getting repaired by RepairSite
+ */
 public class RepairSiteTest {
     private Player player;
-    private List<RepairSite> repairSites;
 
     @Before
     public void setup() {
         player = new Player(0, 0);
-        repairSites = new ArrayList<>();
-        repairSites.add(new RepairSite(5, 2));
+//        repairSites = new ArrayList<>();
+//        repairSites.add(new RepairSite(0, 0));
+        // Oof, player takes one damage before each test
+        player.takeDamage();
+    }
+
+    @Test
+    public void repairOneDamageOnPlayer() {
+        player.repairOneDamage();
+        assertEquals(0, player.getDamage());
     }
 
 
