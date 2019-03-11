ecb3f7b51d8f7f51c3a324c52e0e0a5671dd1667
qez008 <morten_bergmann@live.no>
2019-03-11 16:16:30 +0100

    Add robot start plates to tiled texture

--- assets/gameboard/roborallyTextures.tsx
+++ assets/gameboard/roborallyTextures.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="roborallyTextures" tilewidth="32" tileheight="32" spacing="1" tilecount="30" columns="6">
- <image source="textures.png" width="197" height="164"/>
+<tileset version="1.2" tiledversion="1.2.2" name="roborallyTextures" tilewidth="32" tileheight="32" spacing="1" tilecount="35" columns="7">
+ <image source="textures.png" width="231" height="164"/>
  <tile id="0">
   <properties>
    <property name="Floor" value=""/>
@@ -26,104 +26,154 @@
    <property name="Wall" value="NORTH"/>
   </properties>
  </tile>
+ <tile id="5">
+  <properties>
+   <property name="Start" value="1"/>
+  </properties>
+ </tile>
  <tile id="6">
   <properties>
-   <property name="Hole" value=""/>
+   <property name="Start" value="2"/>
   </properties>
  </tile>
  <tile id="7">
   <properties>
-   <property name="Wall" value="WEST SOUTH"/>
+   <property name="Floor" value="Marking"/>
   </properties>
  </tile>
  <tile id="8">
   <properties>
-   <property name="Wall" value="NORTH WEST"/>
+   <property name="Wall" value="WEST SOUTH"/>
   </properties>
  </tile>
  <tile id="9">
   <properties>
-   <property name="Wall" value="NORTH EAST"/>
+   <property name="Wall" value="NORTH WEST"/>
   </properties>
  </tile>
  <tile id="10">
+  <properties>
+   <property name="Wall" value="NORTH EAST"/>
+  </properties>
+ </tile>
+ <tile id="11">
   <properties>
    <property name="Wall" value="SOUTH EAST"/>
   </properties>
  </tile>
  <tile id="12">
   <properties>
-   <property name="Laser" value="NORTH"/>
+   <property name="Start" value="3"/>
   </properties>
  </tile>
  <tile id="13">
   <properties>
-   <property name="Wall" value="NORTH SOUTH"/>
+   <property name="Start" value="4"/>
   </properties>
  </tile>
  <tile id="14">
   <properties>
-   <property name="Wall" value="EAST WETS"/>
+   <property name="Floor" value="Marking"/>
   </properties>
  </tile>
  <tile id="15">
   <properties>
-   <property name="Wall" value="NORTH SOUTH EAST WEST"/>
+   <property name="Wall" value="NORTH SOUTH"/>
   </properties>
  </tile>
  <tile id="16">
   <properties>
-   <property name="Wall" value=""/>
+   <property name="Wall" value="EAST WETS"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Wall" value="NORTH SOUTH EAST WEST"/>
   </properties>
  </tile>
  <tile id="18">
   <properties>
-   <property name="Laser" value="EAST"/>
+   <property name="Wall" value=""/>
   </properties>
  </tile>
  <tile id="19">
   <properties>
-   <property name="Wall" value="NORTH SOUTH EAST"/>
+   <property name="Start" value="5"/>
   </properties>
  </tile>
  <tile id="20">
   <properties>
-   <property name="Wall" value="NORTH EAST WEST"/>
+   <property name="Start" value="6"/>
   </properties>
  </tile>
  <tile id="21">
   <properties>
-   <property name="Wall" value="NORTH SOUTH WEST"/>
+   <property name="Laser" value="NORTH"/>
   </properties>
  </tile>
  <tile id="22">
   <properties>
-   <property name="Wall" value="SOUTH EAST WEST"/>
+   <property name="Wall" value="NORTH SOUTH EAST"/>
   </properties>
  </tile>
  <tile id="23">
   <properties>
-   <property name="Repair" value=""/>
+   <property name="Wall" value="NORTH EAST WEST"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="Wall" value="NORTH SOUTH WEST"/>
   </properties>
  </tile>
  <tile id="25">
   <properties>
-   <property name="Belt" value="NORTH"/>
+   <property name="Wall" value="SOUTH EAST WEST"/>
   </properties>
  </tile>
  <tile id="26">
   <properties>
-   <property name="Belt" value="EAST"/>
+   <property name="Start" value="7"/>
   </properties>
  </tile>
  <tile id="27">
   <properties>
-   <property name="Belt" value="SOUTH"/>
+   <property name="Start" value="8"/>
   </properties>
  </tile>
  <tile id="28">
   <properties>
+   <property name="Laser" value="EAST"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="Belt" value="NORTH"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="Belt" value="EAST"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="Belt" value="SOUTH"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
    <property name="Belt" value="WEST"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Special" value="Repair"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Special" value="Option"/>
   </properties>
  </tile>
 </tileset>
