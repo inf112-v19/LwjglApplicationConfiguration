2b78d11c624a02a5668152d9558b1c1f25db0854
qez008 <morten_bergmann@live.no>
2019-03-21 15:28:44 +0100

    Add a list of animations to draw
    Finished animations still needs to be removed from the list

--- src/main/java/inf112/roborally/game/screens/Animation.java
+++ src/main/java/inf112/roborally/game/screens/Animation.java
@@ -3,12 +3,19 @@ package inf112.roborally.game.screens;
 import inf112.roborally.game.objects.GameObject;
 
 public abstract class Animation extends GameObject {
+    protected int lifetime;
+    protected int stateTimer;
 
     public Animation(int x, int y, String filePath) {
         super(x, y, filePath);
         makeSprite();
+        updateSprite();
 
     }
 
     protected abstract void update();
+
+    public boolean hasFinished(){
+        return lifetime < stateTimer;
+    }
 }
--- src/main/java/inf112/roborally/game/screens/RepairAnimation.java
+++ src/main/java/inf112/roborally/game/screens/RepairAnimation.java
@@ -2,26 +2,44 @@ package inf112.roborally.game.screens;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.List;
+
 public class RepairAnimation extends Animation {
-    int stateTimer;
+    List<Animation> animations;
+    int rotation;
+    int direction;
 
-    public RepairAnimation(int x, int y) {
+    public RepairAnimation(int x, int y, List<Animation> animations) {
         super(x, y, "assets/animations/wrench.png");
-
+        this.animations = animations;
         sprite.setSize(100, 100);
         sprite.setOriginCenter();
 
         stateTimer = 0;
+        rotation = 45;
+        direction = -1;
+        lifetime = 60;
     }
 
     public void draw(SpriteBatch batch) {
-        update();
         sprite.draw(batch);
+        update();
     }
 
     @Override
     protected void update() {
         updateSprite();
-        sprite.setRotation(90 + 5 * stateTimer++);
+
+        if (rotation < -45) {
+            direction = 1;
+        }
+        else if (rotation > 45) {
+            direction = -1;
+        }
+
+        rotation = rotation + 5 * direction;
+        sprite.setRotation(rotation);
+
+        stateTimer++;
     }
 }
