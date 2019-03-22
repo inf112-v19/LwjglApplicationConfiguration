e89913d473e60b77c10b76f5447afc973a3a5ce5
qez008 <morten_bergmann@live.no>
2019-03-22 16:49:24 +0100

    Add ImageTextButton to testScreen that shows priority

--- src/main/java/inf112/roborally/game/board/GameLogic.java
+++ src/main/java/inf112/roborally/game/board/GameLogic.java
@@ -112,23 +112,23 @@ public class GameLogic {
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
-            player1.executeCard(new ProgramCard(Rotate.NONE, 1, 0));
+            player1.executeCard(new ProgramCard(Rotate.NONE, 1, 0, ""));
             board.boardMoves();
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
-            player1.executeCard(new ProgramCard(Rotate.UTURN, 0, 0));
+            player1.executeCard(new ProgramCard(Rotate.UTURN, 0, 0, ""));
             board.boardMoves();
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
-            player1.executeCard(new ProgramCard(Rotate.RIGHT, 0, 0));
+            player1.executeCard(new ProgramCard(Rotate.RIGHT, 0, 0, ""));
             board.boardMoves();
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
-            player1.executeCard(new ProgramCard(Rotate.LEFT, 0, 0));
+            player1.executeCard(new ProgramCard(Rotate.LEFT, 0, 0, ""));
             board.boardMoves();
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            player1.executeCard(new ProgramCard(Rotate.NONE, 0, 0));
+            player1.executeCard(new ProgramCard(Rotate.NONE, 0, 0, ""));
             board.boardMoves();
         }
 
--- src/main/java/inf112/roborally/game/board/ProgramCard.java
+++ src/main/java/inf112/roborally/game/board/ProgramCard.java
@@ -10,50 +10,58 @@ public class ProgramCard implements Comparable {
     private Rotate rotate;
     private int moveDistance;
     private int priority;
+    private String type;
 
-    public ProgramCard(Rotate rotate, int moveDistance, int priority){
+    public ProgramCard(Rotate rotate, int moveDistance, int priority, String type) {
         this.rotate = rotate;
         this.moveDistance = moveDistance;
         this.priority = priority;
+        this.type = type;
     }
 
-    public int getMoveDistance(){ return this.moveDistance; }
+    public int getMoveDistance() {
+        return this.moveDistance;
+    }
 
-    public Rotate getRotate() { return this.rotate; }
+    public Rotate getRotate() {
+        return this.rotate;
+    }
 
-    public int getPriority(){ return this.priority; }
+    public int getPriority() {
+        return this.priority;
+    }
 
-    public static Stack<ProgramCard> makeProgramCardDeck(){
+    public static Stack<ProgramCard> makeProgramCardDeck() {
         Stack<ProgramCard> cardStack = new Stack<>();
 
         // Adding cards that rotate:
-        for (int priority = 10; priority <= 60; priority+=10) {
+        for (int priority = 10; priority <= 60; priority += 10) {
             // 180
-            cardStack.push(new ProgramCard(Rotate.UTURN, 0, priority));
+            cardStack.push(new ProgramCard(Rotate.UTURN, 0, priority , "uTurn"));
         }
-        for(int priority = 80; priority <= 420; priority+=20){
+        for (int priority = 80; priority <= 420; priority += 20) {
             // Right
-            cardStack.push(new ProgramCard(Rotate.RIGHT,0, priority-10));
+            cardStack.push(new ProgramCard(Rotate.RIGHT, 0, priority - 10, "rotateRight"));
             // Left
-            cardStack.push(new ProgramCard(Rotate.LEFT, 0, priority));
+            cardStack.push(new ProgramCard(Rotate.LEFT, 0, priority, "rotateLeft"));
         }
 
         // Adding cards that move:
         // Backwards
-        for(int priority = 430; priority <= 480; priority+=10){
-            cardStack.push(new ProgramCard(Rotate.NONE, -1, priority));
+        for (int priority = 430; priority <= 480; priority += 10) {
+            cardStack.push(new ProgramCard(Rotate.NONE, -1, priority, "back1"));
         }
         // Forwards 1
-        for(int priority = 490; priority <= 660; priority+=10){
-            cardStack.push(new ProgramCard(Rotate.NONE, 1, priority));
+        for (int priority = 490; priority <= 660; priority += 10) {
+            cardStack.push(new ProgramCard(Rotate.NONE, 1, priority, "move1"));
         }
         // Forwards 2
-        for(int priority = 670; priority <= 780; priority+=10){
-            cardStack.push(new ProgramCard(Rotate.NONE, 2, priority));
+        for (int priority = 670; priority <= 780; priority += 10) {
+            cardStack.push(new ProgramCard(Rotate.NONE, 2, priority, "move2"));
         }
         // Forwards 3
-        for(int priority = 790; priority <= 840; priority+=10){
-            cardStack.push(new ProgramCard(Rotate.NONE, 3, priority));
+        for (int priority = 790; priority <= 840; priority += 10) {
+            cardStack.push(new ProgramCard(Rotate.NONE, 3, priority, "move3"));
         }
         Collections.shuffle(cardStack);
         return cardStack;
@@ -64,20 +72,23 @@ public class ProgramCard implements Comparable {
     public static Stack<ProgramCard> makeTestProgramCardDeck() {
         Stack<ProgramCard> testCardStack = new Stack<>();
         for (int i = 1; i < 27; i++) {
-            testCardStack.push(new ProgramCard(Rotate.RIGHT, 0, i));
+            testCardStack.push(new ProgramCard(Rotate.RIGHT, 0, i, "testCard"));
         }
 
 
         return testCardStack;
     }
 
+    public String getType() {
+        return type;
+    }
 
     @Override
-    public String toString(){
+    public String toString() {
         String s = "";
-        if(this.moveDistance == 0){
-            switch (rotate){
-                case LEFT :
+        if (this.moveDistance == 0) {
+            switch (rotate) {
+                case LEFT:
                     s = "Turn left,";
                     break;
                 case RIGHT:
@@ -95,13 +106,13 @@ public class ProgramCard implements Comparable {
     }
 
     @Override
-    public int compareTo(Object o){
-        if(this.getClass() != o.getClass())
+    public int compareTo(Object o) {
+        if (this.getClass() != o.getClass())
             return 0;
         ProgramCard other = (ProgramCard) o;
-        if(this.getPriority() > other.getPriority())
+        if (this.getPriority() > other.getPriority())
             return -1;
-        return  1;
+        return 1;
 
     }
 
