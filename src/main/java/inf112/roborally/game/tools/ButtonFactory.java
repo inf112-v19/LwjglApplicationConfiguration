15b721fdbc934caaa9160f3dacd9d3fa598dcbf2
qez008 <morten_bergmann@live.no>
2019-04-05 22:55:58 +0200

    Fix typo in method name

--- src/main/java/inf112/roborally/game/tools/ButtonFactory.java
+++ src/main/java/inf112/roborally/game/tools/ButtonFactory.java
@@ -1,7 +1,5 @@
 package inf112.roborally.game.tools;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -11,7 +9,7 @@ import inf112.roborally.game.sound.GameSound;
 
 public class ButtonFactory {
 
-    public static ClickListener clicOnTouchDown(){
+    public static ClickListener soundOnTouchDown(){
         final GameSound sound = new GameSound("assets/soundeffects/buttons_and_clicks/Clic07.mp3");
         return new ClickListener(){
             @Override
@@ -27,7 +25,7 @@ public class ButtonFactory {
         TextureRegionDrawable buttonDown = new TextureRegionDrawable(new Texture(AssMan.RIGHT_ARROW_PRESS.fileName));
         ImageButton arrowRight = new ImageButton(buttonUp, buttonDown);
         arrowRight.setPosition(1920 - arrowRight.getWidth() - 100, 1080 / 2f - arrowRight.getHeight() / 2f);
-        arrowRight.addListener(clicOnTouchDown());
+        arrowRight.addListener(soundOnTouchDown());
         return arrowRight;
     }
 
@@ -36,7 +34,7 @@ public class ButtonFactory {
         TextureRegionDrawable buttonDown = new TextureRegionDrawable(new Texture(AssMan.LEFT_ARROW_PRESS.fileName));
         ImageButton arrowLeft = new ImageButton(buttonUp, buttonDown);
         arrowLeft.setPosition(100, 1080 / 2f - arrowLeft.getWidth() / 2f);
-        arrowLeft.addListener(clicOnTouchDown());
+        arrowLeft.addListener(soundOnTouchDown());
         return arrowLeft;
     }
 
@@ -45,7 +43,7 @@ public class ButtonFactory {
         TextureRegionDrawable buttonDown = new TextureRegionDrawable(new Texture(AssMan.CONFIRM_PRSS.fileName));
         ImageButton confirm = new ImageButton(buttonUp, buttonDown);
         confirm.setPosition(1920 / 2f - confirm.getWidth() / 2, 100);
-        confirm.addListener(clicOnTouchDown());
+        confirm.addListener(soundOnTouchDown());
         return confirm;
     }
 
@@ -54,7 +52,7 @@ public class ButtonFactory {
         TextureRegionDrawable buttonDown = new TextureRegionDrawable(new Texture(AssMan.BACK_PRESS.fileName));
         ImageButton back = new ImageButton(buttonUp, buttonDown);
         back.setPosition(0, 1080 - back.getHeight());
-        back.addListener(clicOnTouchDown());
+        back.addListener(soundOnTouchDown());
         return back;
     }
 }
