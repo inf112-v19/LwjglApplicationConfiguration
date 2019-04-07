1e77cbcb42d954968b0a2af2eb682c1d52a5e6c9
qez008 <morten_bergmann@live.no>
2019-04-07 18:13:20 +0200

    Fix lasers not firing the right way

--- src/main/java/inf112/roborally/game/objects/LaserBeam.java
+++ src/main/java/inf112/roborally/game/objects/LaserBeam.java
@@ -8,7 +8,7 @@ import inf112.roborally.game.tools.AssMan;
 
 import java.util.ArrayList;
 
-public class LaserBeam extends MovableGameObject{
+public class LaserBeam extends MovableGameObject {
     private Board board;
     private ArrayList<Position> beam;
 
@@ -16,34 +16,38 @@ public class LaserBeam extends MovableGameObject{
         super(x, y, "");
         this.board = board;
         beam = new ArrayList<>();
-        setDirection(direction);
+        if (board.getLaserLayer().getCell(position.getX(), position.getY()).getFlipHorizontally()
+                || board.getLaserLayer().getCell(position.getX(), position.getY()).getFlipVertically())
+            setDirection(direction.getOppositeDirection());
+        else setDirection(direction);
         sprite = AssMan.manager.get(AssMan.FLAG_ATLAS).createSprite("1");
-        sprite.setBounds(0,0,32,32);
+        sprite.setBounds(0, 0, 32, 32);
     }
 
     @Override
-    public void draw(SpriteBatch batch){
-        update();
-        for(Position pos : beam){
-            sprite.setPosition(pos.getX()*32, pos.getY()*32);
+    public void draw(SpriteBatch batch) {
+        beam.clear();
+        update(getDirection());
+        for (Position pos : beam) {
+            sprite.setPosition(pos.getX() * 32, pos.getY() * 32);
             sprite.draw(batch);
         }
     }
 
-    private void update() {
-        beam.clear();
+    private void update(Direction direction) {
         if (beamBlockedByRobot()) return;
         MovableGameObject laserbeam = new MovableGameObject(getX(), getY(), "");
-        laserbeam.setDirection(this.getDirection());
-        while (laserbeam.canGo(laserbeam.getDirection(),board.getWallLayer())){
+        laserbeam.setDirection(direction);
+        while (true) {
             if (laserbeam.crashWithRobot(laserbeam.getDirection(), board)) return;
+            if (!laserbeam.canGo(laserbeam.getDirection(), board.getWallLayer())) return;
             laserbeam.moveInDirection(getDirection());
             beam.add(new Position(laserbeam.getX(), laserbeam.getY()));
         }
     }
 
     private boolean beamBlockedByRobot() {
-        for(Player robot : board.getPlayers()){
+        for (Player robot : board.getPlayers()) {
             if (position.equals(robot.position)) return true;
         }
         return false;
