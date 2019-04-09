a69b477799ea45867b0ce0495fe4a886bb19a407
qez008 <morten_bergmann@live.no>
2019-04-09 16:38:40 +0200

    Add click listener to next and previous

--- src/main/java/inf112/roborally/game/screens/setup/AbstractSelectionScreen.java
+++ src/main/java/inf112/roborally/game/screens/setup/AbstractSelectionScreen.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import inf112.roborally.game.RoboRallyGame;
 import inf112.roborally.game.tools.ButtonFactory;
 
@@ -16,18 +18,34 @@ public abstract class AbstractSelectionScreen implements Screen {
     protected Button previous;
     protected Button confirm;
     protected Button back;
+    protected int index;
+    protected int maxIndex;
 
     public AbstractSelectionScreen(RoboRallyGame game) {
         this.game = game;
         stage = new Stage(game.fixedViewPort, game.batch);
         next = ButtonFactory.createArrowRightButton();
+        next.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                index = ++index % maxIndex;
+            }
+        });
         previous = ButtonFactory.createArrowLeftButton();
+        previous.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                index = (--index + maxIndex) % maxIndex;
+            }
+        });
         confirm = ButtonFactory.createConfirmButton();
         back = ButtonFactory.createBackButton();
         stage.addActor(next);
         stage.addActor(previous);
         stage.addActor(confirm);
         stage.addActor(back);
+        index = 0;
+        maxIndex = 1; // increase this in subclass if it has more than 2 options.
     }
 
     @Override
@@ -43,7 +61,7 @@ public abstract class AbstractSelectionScreen implements Screen {
         stage.draw();
     }
 
-    protected void handleInput(){
+    protected void handleInput() {
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
     }
 
@@ -58,8 +76,6 @@ public abstract class AbstractSelectionScreen implements Screen {
     }
 
     // Unimplemented methods
-
-
 
 
     @Override
