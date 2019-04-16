29ee7e4917341e7e4d26d388fdec8977eb839ea3
Simen <nip009@uib.no>
2019-04-17 00:42:05 +0200

    Remove unnecessary variables

--- src/main/java/inf112/roborally/game/screens/setup/SelectMapScreen.java
+++ src/main/java/inf112/roborally/game/screens/setup/SelectMapScreen.java
@@ -2,13 +2,15 @@ package inf112.roborally.game.screens.setup;
 
 import com.badlogic.gdx.graphics.Texture;
 import inf112.roborally.game.RoboRallyGame;
-import inf112.roborally.game.board.Board;
-import inf112.roborally.game.enums.SetupState;
 import inf112.roborally.game.tools.AssMan;
 
+/**
+ * Screen for selecting which map to use.
+ *
+ * {@link #choiceIndex} is the index for which map to use.
+ */
 public class SelectMapScreen extends SelectScreen {
 
-    protected int mapChoiceIndex;
     protected Texture mapTexture;
 
     private PlaceFlagsScreen placeFlagsScreen;
@@ -17,19 +19,15 @@ public class SelectMapScreen extends SelectScreen {
         super(game, AssMan.getMapChoices(), AssMan.getMapChoices().length);
     }
 
+
     @Override
     public void completeChoice() {
-        mapChoiceIndex = choiceIndex;
-        mapTexture = AssMan.getMapChoices()[mapChoiceIndex];
-        game.board.createBoard(game.chosenMap(mapChoiceIndex));
+        mapTexture = AssMan.getMapChoices()[choiceIndex];
+        game.board.createBoard(game.chosenMap(choiceIndex));
         game.board.findLaserGuns();
         this.placeFlagsScreen = new PlaceFlagsScreen(game);
         game.setScreen(placeFlagsScreen);
         dispose();
-    }
-
-    public int getMapIndex(){
-        return mapChoiceIndex;
     }
 
     public Texture getMapTexture(){
--- src/main/java/inf112/roborally/game/screens/setup/SelectSkinScreen.java
+++ src/main/java/inf112/roborally/game/screens/setup/SelectSkinScreen.java
@@ -6,21 +6,20 @@ import inf112.roborally.game.tools.AssMan;
 
 import static inf112.roborally.game.enums.Direction.NORTH;
 
-public class SelectSkinScreen extends SelectScreen {
 
-    protected int skinChoiceIndex;
+/**
+ * Screen for selecting a player skin.
+ *
+ * {@link #choiceIndex} is the index for the chosen player skin.
+ */
+public class SelectSkinScreen extends SelectScreen {
 
     public SelectSkinScreen(final RoboRallyGame game) {
         super(game, AssMan.getPlayerSkins(), AssMan.getPlayerSkins().length);
     }
 
-
-    /**
-     * {@link #choiceIndex} is the skin index for the chosen player skin.
-     */
     @Override
     public void completeChoice() {
-        skinChoiceIndex = choiceIndex;
         for (Player player : game.board.getPlayers()) {
             player.setSkinTexture(AssMan.getPlayerSkins()[choiceIndex]);
         }
@@ -30,7 +29,7 @@ public class SelectSkinScreen extends SelectScreen {
     }
 
     public void addPlayersToBoard() {
-        int index = game.selectSkinScreen.skinChoiceIndex;
+        int index = choiceIndex;
         int numberOfSkins = AssMan.getPlayerSkins().length;
         for (int i = 0; i < numberOfSkins; i++) {
             if (index >= numberOfSkins) {
