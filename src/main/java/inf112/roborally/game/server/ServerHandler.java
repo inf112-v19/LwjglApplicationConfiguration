facb5846f77086011276b0001ed07ee92bb23f62
William Ye <crimana08@gmail.com>
2019-05-01 19:15:35 +0200

    Server now deal cards with commands

--- src/main/java/inf112/roborally/game/server/ServerHandler.java
+++ src/main/java/inf112/roborally/game/server/ServerHandler.java
@@ -2,6 +2,7 @@ package inf112.roborally.game.server;
 
 import inf112.roborally.game.RoboRallyGame;
 import inf112.roborally.game.objects.Position;
+import inf112.roborally.game.player.ProgramCard;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -9,12 +10,20 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
+import java.util.Stack;
+
+import static java.util.Collections.shuffle;
+
 public class ServerHandler extends SimpleChannelInboundHandler<String> {
 
     private final RoboRallyGame game;
+    protected Stack<ProgramCard> returnedProgramCards;
+    protected Stack<ProgramCard> stackOfProgramCards;
 
     public ServerHandler(RoboRallyGame game) {
         this.game = game;
+        stackOfProgramCards = ProgramCard.makeProgramCardDeck();
+        returnedProgramCards = new Stack<>();
     }
 
     public static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
@@ -47,25 +56,53 @@ public class ServerHandler extends SimpleChannelInboundHandler<String> {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 
-            String packet = msg.toString();
-            String[] split = packet.split(" ", 2);
-            String header = split[0];
+        String packet = msg.toString();
+        String[] split = packet.split(" ", 2);
+        String header = split[0];
 
-            if (header.equals("HANDSHAKE")) {
+        switch (header) {
+            case "HANDSHAKE":
                 System.out.println("[SERVER] " + split[1] + " has connected!");
                 game.playerNames.add(split[1]);
 
                 for (Channel channel : channels) {
                     channel.writeAndFlush(split[1] + " has connected!" + "\r\n");
                 }
-
-            } else {
+                break;
+            case "REQUEST_CARDS":
+                String name = split[2];
+                int numbers = Integer.parseInt(split[1]);
+                for (int i = 0; i < numbers; i++) {
+                    if (stackOfProgramCards.isEmpty()) { // in case the game drags on and we run out of cards - Morten
+                        reshuffleDeck();
+                    }
+                    for (Channel channel : channels) {
+                        channel.writeAndFlush("RECEIVE_CARDS " + name + " " + stackOfProgramCards.pop().toString() + "\r\n");
+                    }
+                }
+                break;
+            case "CARD":
+                ProgramCard card = new ProgramCard(split[2], split[3], split[4]);
+                returnedProgramCards.add(card);
                 for (Channel channel :
                         channels) {
                     channel.writeAndFlush(split[0] + " " + split[1] + "\r\n");
                 }
-            }
+                break;
+            default:
+                for (Channel channel :
+                        channels) {
+                    channel.writeAndFlush(split[0] + " " + split[1] + "\r\n");
+                }
+
+                break;
         }
+    }
+    private void reshuffleDeck() {
+        while (!returnedProgramCards.empty())
+            stackOfProgramCards.push(returnedProgramCards.pop());
+        shuffle(stackOfProgramCards);
+    }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
