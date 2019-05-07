inf112-v19/LwjglApplicationConfiguration

commit 37fa963fe6eb7f43c457a336d61c272ed2792ca4

Fix NullPointer when winning with 8 flags

src/main/java/inf112/roborally/game/gui/Hud.java
@@ -163,7 +163,9 @@ public class Hud {
             clearButton.setVisible(false);
         }
 
-        stage.draw();
+        if(game.gameScreen.getBoardLogic().getState() != GameState.GAME_OVER) {
+            stage.draw();
+        }
     }
 
     private void clearAllCardButtons() {

commit b217b4e7ff7d7d812e826c927261b0b78cc3f88a

Make GuiTest and BoardTest not give nullpointer
Fix nullpointer caused by change made in
37fa963fe6eb7f43c457a336d61c272ed2792ca4

src/main/java/inf112/roborally/game/gui/Hud.java
@@ -163,6 +163,12 @@ public class Hud {
             clearButton.setVisible(false);
         }
 
+        //To make GuiTest and BoardTest work
+        if(game.gameScreen == null){
+            stage.draw();
+            return;
+        }
+
         if(game.gameScreen.getBoardLogic().getState() != GameState.GAME_OVER) {
             stage.draw();
         }